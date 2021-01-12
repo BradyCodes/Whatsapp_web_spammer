d2d3a9e01fe32bd921e9988e5efcf1ef191a31fd
Wout Vos <65921049+WoutVos@users.noreply.github.com>
2021-01-12 16:03:04 +0100

    Update whatsapp_spam_bot.py

--- whatsapp_spam_bot.py
+++ whatsapp_spam_bot.py
@@ -6,27 +6,41 @@ import time
 # Define some stuff
 browser  = webdriver.Chrome(ChromeDriverManager().install())
 ready_message = "go ahead"
+ready_to_send_message = "send"
 
 # Start the browser and go to the site
 browser.get('https://web.whatsapp.com')
 
 # Check if the user is ready
-ready = input("If you want to go ahead and you have scanned the qr code type 'go ahead'")
+ready = input("If you want to go ahead and you have scanned the qr code type '" + ready_message + "' ")
 
 while True:
     if ready == ready_message:
         break
 
-# Print if it is working
-print("logged in")
+# Define "send()"
+def send():
+    # Choose the person
+    browser.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
 
-# Choose the person
-browser.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
+    # Enter the message
+    inputString = input("Enter message to send: ")
 
-# Enter the message
-inputString = input("Enter message to send: ")
+    # Send the messages
+    try:
+        while True:
+            browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
+            browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
 
-# Send the messages
+    except KeyboardInterrupt:
+        print("Press Ctrl-C to terminate while statement")
+        pass
+
+
+# Choose what to do
+#what_do_you_want_to_do = input("What do you want to do? ")
+
+# Spam
 while True:
-    browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
-    browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+    if input("What do you want to do? ") == "send":
+        send()
